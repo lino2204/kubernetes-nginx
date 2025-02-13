eadbebaac7fa8c20eb80ac7ba3489064cb7f3177
Lino <lino.padilla.delacruz@gmail.com>
2025-02-13 14:47:53 -0500

    fix main terraform configuration

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -1,10 +1,10 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.4.0"  # Ensure you use the latest supported version
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = "kubernetes-nginx"
   cluster_version = "1.27"
-  subnet_ids      = ["subnet-xxxxxx", "subnet-yyyyyy", "subnet-zzzzzz"]
-  vpc_id          = "vpc-xxxxxx"
+  subnet_ids      = ["subnet-0dc05aad49e7d2683", "subnet-03363be1ce4604a62"]
+  vpc_id          = "vpc-0c30330460fa51680"
 
   eks_managed_node_groups = {
     eks_nodes = {
